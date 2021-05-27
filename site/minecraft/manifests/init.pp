45c0cf5d37d9900c4e802a4a8bdff1ba59da1b61
peromorrison <pero_morrison@hotmail.com>
2021-05-27 11:17:05 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,10 +1,12 @@
 class minecraft {
+   $url = 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar'
+   $install_dir = '/opt/minecraft/'
    file {'/opt/minecraft':
     ensure => directory,
    }
-   file {'/opt/minecraft/minecraft_server.jar':
+   file {"${install_dir}/minecraft_server.jar":
      ensure => file,
-     source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+     source => $url,
      before => Service['minecraft'],
     }     
     package {'java':
