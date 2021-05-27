ab3f25cd6b9652da2a77ac1286252437a8259785
peromorrison <pero_morrison@hotmail.com>
2021-05-27 10:57:00 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,1 +1,24 @@
-
+class minecraft {
+   file {'/opt/minecraft':
+    ensure => directory,
+   }
+   file {'/opt/minecraft/server.jar':
+     ensure => file,
+     source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+    }     
+    package {'java':
+     ensure => present,
+    }
+    file {'/opt/minecraft/eula.txt':
+      ensure  => present,
+      content => 'eula=true'
+     }
+     file {'/etc/systemd/system/minecraft.service':
+       ensure => present,
+       source => 'puppet:///modules/minecraft/minecraft.service',
+     }
+     service {'minecraft':
+      ensure => running,
+      enable => true,
+     }
+}
