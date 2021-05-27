a8040235d0a389a46b66ff702f4ba3532dccecc6
peromorrison <pero_morrison@hotmail.com>
2021-05-27 09:44:03 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDThhpr3//mPXYRHA9djzIQMIp2VR4ZiAuce6PYiYhkjXu9uhhv5IQa5UbjlXEDNtBzV4TCuNVK+8F9PVg8zf4hf/nFvmn/qBe1kfxvgwDMb1DQ+WlE2hhiClv0GdGQShFXTYhJLD46DokYL9qRz2xhJcRtErMyyoxeIOcnNAsmWsoNRKk5ePktmvyOvqNaupPz7eJNB+Ej4oGUI3ku2tjvBoSrAPhp+HwznkwPRoYEPm31vNk1qmj3it/RsXD70cNerzuSt4w6LickqUPSYi5rMUth5h34l0jVpiJlK2kNZP7GXohz7TG1pyDNZlmtuYs9Uc8eDQ4U+KylBrvuHOc7',
 	}  
 }
